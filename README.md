478da055ed5e3408841686206cd0dab9bcf944e9
Peter Limawal <peterlimawal007@gmail.com>
2024-02-22 23:16:57 -0500

    Implement 3/4 APIs

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# htn-2024-backend
+# Hack the North 2024 Backend Challenge - Peter Limawal
+
+### All Users Endpoint:
+http://127.0.0.1:5000/get/hackers/all\
+Returns a list of all user data from the database in a JSON format
+
+### User Information Endpoint:
+http://127.0.0.1:5000/get/hackers/\<email>\
+Returns user data with email \<email> from the database in a JSON format  
+
+### Skills Endpoints:
+http://127.0.0.1:5000/get/skills/frequencies\
+Returns number of users with each skill (frequency) in a JSON format
+
+http://127.0.0.1:5000/get/skills?min_frequency=\<X>&max_frequency=\<Y>\
+Query parameter filtering - Returns number of users with each skill (frequency) between minimum \<X> and maximum \<Y> in a JSON format
---
+++ main.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, jsonify
+import json
+import sqlite3
+conn = sqlite3.connect(':memory:', check_same_thread=False)
+
+app = Flask(__name__)
+c = conn.cursor()
+
+# Create hackers table
+c.execute("""CREATE TABLE hackers (
+            name text,
+            company text,
+            email text,
+            phone text
+            )""")
+# Create skills table
+c.execute("""CREATE TABLE skills(
+            email text,
+            skill text,
+            rating integer
+            )""")
+
+def add_new_hacker(hacker):
+    with conn:
+        c.execute("INSERT INTO hackers VALUES (:name, :company, :email, :phone)", {'name': hacker["name"], 'company': hacker["company"], 'email': hacker["email"], 'phone': hacker["phone"]})
+        for hacker_skill in hacker["skills"]:
+            c.execute("INSERT INTO skills VALUES (:email, :skill, :rating)", {'email': hacker["email"], 'skill': hacker_skill["skill"], 'rating': hacker_skill["rating"]})
+
+
+# Import json file into database
+with open('HTN_2023_BE_Challenge_Data.json', 'r') as json_file:
+    data = json.load(json_file)
+    for item in data:
+        add_new_hacker(item)
+
+conn.commit()
+
+@app.route("/")
+def home():
+    return "Home"
+
+# All Users Endpoint
+@app.route("/get/hackers/all", methods=['GET'])
+def get_all_hackers():
+    c.execute("SELECT * FROM hackers")
+    hacker_list = c.fetchall()
+
+    return_list = []
+    for hacker in hacker_list:
+        tmp = list(hacker)
+        c.execute("SELECT skill, rating FROM skills WHERE email=:email", {'email': tmp[2]})
+        tmp.append(c.fetchall())
+        return_list.append(tmp)
+
+    return jsonify(return_list), 200
+
+# User Information Endpoint
+@app.route("/get/hackers/<email>", methods=['GET'])
+def get_user(email):
+    c.execute("SELECT * FROM hackers WHERE email=:email", {'email': email})
+    hacker = list(c.fetchall())
+    c.execute("SELECT skill, rating FROM skills WHERE email=:email", {'email': hacker[0][2]})
+    hacker.append(c.fetchall())
+    return jsonify(hacker), 200
+
+# Skills Endpoints
+@app.route("/get/skills/frequencies", methods=['GET'])
+def get_skills():
+    c.execute("SELECT skill, COUNT(*) FROM skills GROUP BY skill")
+    return jsonify(c.fetchall()), 200
+
+@app.route("/get/skills", methods=['GET'])
+def get_skills_filter():
+    min_freq = int(request.args.get("min_frequency"))
+    max_freq = int(request.args.get("max_frequency"))
+    c.execute("SELECT skill, COUNT(*) FROM skills GROUP BY skill HAVING COUNT(skill) BETWEEN :min_freq AND :max_freq", {'min_freq': min_freq, 'max_freq': max_freq})
+    return jsonify(c.fetchall()), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+conn.close()
